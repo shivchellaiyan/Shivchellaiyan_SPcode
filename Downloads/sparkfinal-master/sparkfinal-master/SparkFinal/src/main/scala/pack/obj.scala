a3d61387830c7c50cf5d949a86d25d29099c7cf5
shivchellaiyan <sivachellaiyan@gmail.com>
2024-01-06 01:07:12 +0530

    Shivspark
    Shivspark

---
+++ Downloads/sparkfinal-master/sparkfinal-master/SparkFinal/src/main/scala/pack/obj.scala
@@ -0,0 +1,18 @@
+package pack
+
+object obj {
+  
+  def main(args:Array[String]):Unit={
+    
+    
+    println("===1===")
+    
+    println("===2===")
+    
+    println("===3====")
+    
+    println("===4====")
+    
+  }
+  
+}
